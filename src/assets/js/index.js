2b4d0938c411da719591b4dc0fa6e9e2cbc88934
oleksandr <2356931k@gmail.com>
2022-06-01 20:30:52 +0300

    commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,28 +1,28 @@
-// class Cart {
-//     constructor() {
-//         this.cart = [];
-//         this.items = items = [{
-//             id: 1,
-//             name: 'Dove Soap',
-//             price: 39.99
-//         }, {
-//             id: 2,
-//             name: 'Axe Deo',
-//             price: 99.99
-//         }];
-//     }
+class Cart {
+    constructor() {
+        this.cart = [];
+        this.items = items = [{
+            id: 1,
+            name: 'Dove Soap',
+            price: 39.99
+        }, {
+            id: 2,
+            name: 'Axe Deo',
+            price: 99.99
+        }];
+    }
 
 
-//     getItems() {
-//         return this.items;
-//     }
+    getItems() {
+        return this.items;
+    }
 
 
-// }
+}
 
-// console.log(Cart);
+console.log(Cart);
 
-// export { Cart };
+export { Cart };
 
 let a = 5;
 let b = 5;
