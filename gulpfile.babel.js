a45cf2ec2cd53b05be4cc4be4c31f662a21030a2
oleksandr <2356931k@gmail.com>
2022-06-01 18:14:31 +0300

    fix prod version

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,8 +18,10 @@ const autoprefixer = require('gulp-autoprefixer');
 const mode = require('gulp-mode')();
 const htmlmin = require('gulp-htmlmin');
 
-//шлях на локальній сервак
+//шлях до папок
 const localServer = {
+  src: 'src/',
+  assets: './src/assets/',
   out: './dist/',
   port: 9091,
   url: 'http://localhost:',
@@ -27,19 +29,19 @@ const localServer = {
 
 // Просто додає html в папку dist/ і додає include щоб можна було писати відносний шлях до файлів і ще багато чтого читати тут https://www.npmjs.com/package/gulp-file-include
 function html() {
-  return src('src/**/*.html')
+  return src(`${localServer.src}**/*.html`)
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(mode.production(htmlmin({ collapseWhitespace: true })))
+    .pipe(mode.production(htmlmin({ collapseWhitespace: true, removeComments: true })))
     .pipe(dest(localServer.out))
     .pipe(connect.reload());;
 };
 
 //функція яка всі css файли додає в 1 і компілює scss в css і сжимає його
 function css() {
-  return src('./src/sass/main.scss')
+  return src(`${localServer.assets}sass/main.scss`)
     .pipe(sourceMap.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(mode.production(miniFycss()))
@@ -53,8 +55,8 @@ function css() {
 
 //функція яка додає JS файл в папку dist/ і робить всі плюшки ES6 плюс сжимає файл
 function js() {
-  return src('./src/**/*.js')
-    .pipe(changed('./dist/js/*.js'))
+  return src(`${localServer.assets}**/*.js`)
+    .pipe(changed(`${localServer.out}js/*.js`))
     .pipe(sourceMap.init())
     .pipe(babel({
       presets: ['@babel/preset-env', "@babel/react"],
@@ -68,29 +70,29 @@ function js() {
 
 //Функція лінт яка показує помилки якшо вони є
 function lint() {
-  return src('./src/js/*.js')
+  return src(`${localServer.assets}js/*.js`)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 }
 
 //Функція яказа закидуе картинки в папку dist/ і ії сжимає автоматом
 function img() {
-  return src('src/img/**/*')
-    .pipe(changed('./dist/img/'))
+  return src(`${localServer.assets}img/**/*`)
+    .pipe(changed(`${localServer.out}img/`))
     .pipe(imagemin())
     .pipe(dest(`${localServer.out}img`))
 };
 
 //Ватч функція яка слідкуя за зміною в файлах в лайв режимі
 function gulpWatch() {
-  watch('./src/**/*.html', html);
-  watch('./src/sass/**/*.scss', css);
-  watch('./src/js/**/*.js', js);
-  watch('./src/img/**/*', img);
+  watch(`${localServer.src}**/*.html`, html);
+  watch(`${localServer.assets}sass/**/*.scss`, css);
+  watch(`${localServer.assets}js/**/*.js`, js);
+  watch(`${localServer.assets}img/**/*`, img);
 }
 //Функція яка очіщає папку dist/ кожного разу коли ви стартуете npm start це щоб чистити кеш
 function clean() {
-  return del(['dist/**', '!dist'])
+  return del([`${localServer.out}**`, `${!localServer.out}`])
 }
 //Це локальний сервак
 function server() {
@@ -102,7 +104,7 @@ function server() {
 }
 //Автоматов відкриває в браузері, по дефолту я не додавай в exports.dev
 function openLocal() {
-  return src('./dist/index.html')
+  return src(`${localServer.out}index.html`)
     .pipe(open({ uri: `${localServer.url}${localServer.port}/` }))
 }
 
