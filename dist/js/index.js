f8598e8db201ed785b616c27be6c9bb3de214baf
oleksandr <2356931k@gmail.com>
2022-12-05 21:12:05 +0200

    sasdas

--- dist/js/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";document.querySelector("#btn1").onclick=function(){document.querySelector(".home-page__modal-container").classList.add("is-visible")},document.querySelector(".home-page__modal-close").onclick=function(){document.querySelector(".home-page__modal-container").classList.remove("is-visible")};
-//# sourceMappingURL=index.js.map
